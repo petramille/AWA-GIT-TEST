8ae4cd6d7fa25f13a97df08b0ecd42358b418d11
Petra Mille <mille.petra@gmail.com>
2015-09-24 11:51:33 +0200

    Nu har jag skrivit i en branch

--- GITTest/GITTest/Program.cs
+++ GITTest/GITTest/Program.cs
@@ -18,13 +18,17 @@ namespace GITTest
             Console.WriteLine("OCh så lite till");
             Console.WriteLine("Ett försök till");
             Console.WriteLine("Överlever den här mergen?");
-            //Jag tog bort en rad här
+            Console.WriteLine("Det här är en rad i branchen");
         }
 
-    static void TestMethod()
+        static void TestMethod()
         {
             Console.WriteLine("Ny utbytt rad i den nya metoden");
-            //Det här är en rad i den nya branchen
+        }
+
+        static void NewMethod()
+        {
+
         }
     }
 }
