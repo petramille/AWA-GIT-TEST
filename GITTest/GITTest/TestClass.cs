664c79a4710b31534c5cd85e8d372f1e75d22411
Michelle Sjöö <michelle.sjoo@gmail.com>
2015-09-24 10:39:58 +0200

    Nu finns en class med

---
+++ GITTest/GITTest/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITTest
+{
+    class TestClass
+    {
+        public string ClassMember { get; set; }
+    }
+}
